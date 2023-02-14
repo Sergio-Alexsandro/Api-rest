ebecafe58d61c1058e57eed48dd6ce848c1d5d05
Sergio Alexsandro <sgp.07adm@gmail.com>
2023-02-14 10:07:32 -0300

    inicial

---
+++ src/database/seeds/20230116141332-criar-usuarios.js
@@ -0,0 +1,39 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Luiz',
+        email: 'luiz@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      },
+
+      {
+        nome: 'Luiz2',
+        email: 'luiz2@gmail.com',
+        password_hash: await bcryptjs.hash('456789', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      },
+
+      {
+        nome: 'Luiz3',
+        email: 'luiz3@gmail.com',
+        password_hash: await bcryptjs.hash('789123', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      },
+
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
